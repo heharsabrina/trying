3b297d03b65faad69f38c6e427a19112d29dc64c
heharsabrina <120552555+heharsabrina@users.noreply.github.com>
2022-12-29 21:31:40 +0530

    Update trying.c

--- trying.c
+++ trying.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 void main()
 {
-    int a,b=10,sum=2;
+    int a,b=10,sum=2,x=39;
     printf("Enter two numbers:");
     scanf("%d",&a);
     sum=a+b+1;
-    printf("Sum=%d",sum);
+    printf("Sum=%d %d",sum,x);
 }
